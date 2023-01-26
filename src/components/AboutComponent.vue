c63774938ca7deb1b7e1db53a0c8c21af987dc5c
Dev-HyunSang <me@hyunsang.dev>
2023-01-26 23:20:56 +0900

    docs: update docs

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -15,7 +15,7 @@
         <p style="padding-top: 0.5rem;">
             <b>Inspired Photography</b><br/>
             <a href="https://shinnoguchiphotography.com/">Shin Noguchi</a>, <a href="https://changwlee.com/">Chang W. Lee</a>, <a href="http://todd-heisler.squarespace.com/">Todd Heisler</a>, <a href="https://www.instagram.com/kim_reuters_photojournalist/">Kyung-Hoon Kim (김경훈)</a>, <a href="https://www.instagram.com/lim_byeongsik_photos/">임병식(Lim Byeongsik)</a><br/>
-            <a href="http://www.vivianmaier.com/gallery/street-1/">Vivian Maier</a>, <a href="https://www.philpenman.com/">Phil Penman</a>, <a href="http://www.kyohnam.com/">Kyo H Nam</a>
+            <a href="http://www.vivianmaier.com/gallery/street-1/">Vivian Maier</a>, <a href="https://www.philpenman.com/">Phil Penman</a>, <a href="http://www.kyohnam.com/">Kyo H Nam</a>, <a href="http://alanschaller.com/">Alan Schaller</a>
         </p>
         <p style="padding-top: 0.5rem;">
             <b>Email.</b> <a href="mailto:me@hyunsang.dev">me@hyunsang.dev</a> | <a href="mailto:parkhyunsang@kakao.com">parkhyunsang@kakao.com</a>
